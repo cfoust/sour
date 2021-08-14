cb3f0fe1bba1bd8f0bbf03f007e445d63383f303
Caleb Foust <cfoust@sqweebloid.com>
2021-08-14 11:01:01 -0700

    Change to preload complex

--- cube2/mini-worker.js
+++ cube2/mini-worker.js
@@ -25,5 +25,5 @@ importScripts("game/gl-matrix.js");
 importScripts("game/setup_low.js");
 importScripts("game/preload_base.js");
 importScripts("game/preload_character.js");
-importScripts("game/preload_low.js");
+importScripts("game/preload_complex.js");
 
