1694a9f6706f8e2e56e7820fc5b265d2236a8639
Caleb Foust <cfoust@sqweebloid.com>
2021-08-18 17:25:36 -0700

    Move canvas creation into react land

--- services/client/src/unsafe-startup.js
+++ services/client/src/unsafe-startup.js
@@ -1,8 +1,4 @@
 export default function start() {
-  var statusElement = document.getElementById('status')
-  var progressElement = document.getElementById('progress')
-  var spinnerElement = document.getElementById('spinner')
-
   Module = {
     ...Module,
     preRun: [],
@@ -21,47 +17,26 @@ export default function start() {
         console.error(text)
       }
     },
-    canvas: (function () {
-      var canvas = document.getElementById('canvas')
-
-      // As a default initial behavior, pop up an alert when webgl context is lost. To make your
-      // application robust, you may want to override this behavior before shipping!
-      // See http://www.khronos.org/registry/webgl/specs/latest/1.0/#5.15.2
-      canvas.addEventListener(
-        'webglcontextlost',
-        function (e) {
-          alert('WebGL context lost. You will need to reload the page.')
-          e.preventDefault()
-        },
-        false
-      )
-
-      canvas.addEventListener('click', function () {
-        canvas.requestPointerLock()
-      })
-
-      return canvas
-    })(),
     setStatus: function (text) {
-      if (!Module.setStatus.last)
-        Module.setStatus.last = { time: Date.now(), text: '' }
-      if (text === Module.setStatus.text) return
-      var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/)
-      var now = Date.now()
-      if (m && now - Date.now() < 30) return // if this is a progress update, skip it if too soon
-      if (m) {
-        text = m[1]
-        progressElement.value = parseInt(m[2]) * 100
-        progressElement.max = parseInt(m[4]) * 100
-        progressElement.hidden = false
-        spinnerElement.hidden = false
-      } else {
-        progressElement.value = null
-        progressElement.max = null
-        progressElement.hidden = true
-        if (!text) spinnerElement.style.display = 'none'
-      }
-      statusElement.innerHTML = text
+      //if (!Module.setStatus.last)
+        //Module.setStatus.last = { time: Date.now(), text: '' }
+      //if (text === Module.setStatus.text) return
+      //var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/)
+      //var now = Date.now()
+      //if (m && now - Date.now() < 30) return // if this is a progress update, skip it if too soon
+      //if (m) {
+        //text = m[1]
+        //progressElement.value = parseInt(m[2]) * 100
+        //progressElement.max = parseInt(m[4]) * 100
+        //progressElement.hidden = false
+        //spinnerElement.hidden = false
+      //} else {
+        //progressElement.value = null
+        //progressElement.max = null
+        //progressElement.hidden = true
+        //if (!text) spinnerElement.style.display = 'none'
+      //}
+      //statusElement.innerHTML = text
     },
     totalDependencies: 0,
     monitorRunDependencies: function (left) {
@@ -96,12 +71,12 @@ export default function start() {
     }
   }
 
-  const box = document.getElementById("box");
-  Module.desiredWidth = box.clientWidth;
-  Module.desiredHeight = box.clientHeight
-  window.onresize = () => {
-    Module.setCanvasSize(window.innerWidth, window.innerHeight)
-  }
+  //const box = document.getElementById("box");
+  //Module.desiredWidth = box.clientWidth;
+  //Module.desiredHeight = box.clientHeight
+  //window.onresize = () => {
+    //Module.setCanvasSize(window.innerWidth, window.innerHeight)
+  //}
 
   Module.autoexec = function () {
     Module.setStatus("");
