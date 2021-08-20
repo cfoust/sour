85eac9901a932c9fb96876635ffa47dc861aac2f
Caleb Foust <cfoust@sqweebloid.com>
2021-08-19 17:32:54 -0700

    Basic module state tracking in React!

--- services/client/definitions/global.d.ts
+++ services/client/definitions/global.d.ts
@@ -1,9 +1,17 @@
 // Lets us use the Module API in a type safe way
 type ModuleType = {
+  tweakDetail: () => void
+  postLoadWorld: () => void
   desiredWidth: number
   desiredHeight: number
   canvas: HTMLCanvasElement | null
   setCanvasSize: ((width: number, height: number) => void) | null
   setStatus: (text: string) => void
+  print: (text: string) => void
 }
 declare const Module: ModuleType
+
+type BananaBreadType = {
+  execute: (command: string) => void
+}
+declare const BananaBread: BananaBreadType
