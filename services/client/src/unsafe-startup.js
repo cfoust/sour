22592616b52027c6cc3283c9c5f4a20a6fbaa930
Caleb Foust <cfoust@sqweebloid.com>
2021-08-20 14:47:57 -0700

    Hide pointless log lines

--- services/client/src/unsafe-startup.js
+++ services/client/src/unsafe-startup.js
@@ -4,35 +4,15 @@ export default function start() {
     preRun: [],
     postRun: [],
     printErr: function (text) {
-      text = Array.prototype.slice.call(arguments).join(' ')
-      if (0) {
-        // XXX disabled for safety typeof dump == 'function') {
-        dump(text + '\n') // fast, straight to the real console
-      } else {
-        console.error(text)
-      }
+      if (
+        // These two happen a lot while playing and they don't mean anything.
+        text.startsWith('Cannot find preloaded audio') ||
+        text.startsWith("Couldn't find file for:")
+      )
+        return
+      console.error(text)
     },
-    setStatus: function (text) {
-      //if (!Module.setStatus.last)
-      //Module.setStatus.last = { time: Date.now(), text: '' }
-      //if (text === Module.setStatus.text) return
-      //var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/)
-      //var now = Date.now()
-      //if (m && now - Date.now() < 30) return // if this is a progress update, skip it if too soon
-      //if (m) {
-      //text = m[1]
-      //progressElement.value = parseInt(m[2]) * 100
-      //progressElement.max = parseInt(m[4]) * 100
-      //progressElement.hidden = false
-      //spinnerElement.hidden = false
-      //} else {
-      //progressElement.value = null
-      //progressElement.max = null
-      //progressElement.hidden = true
-      //if (!text) spinnerElement.style.display = 'none'
-      //}
-      //statusElement.innerHTML = text
-    },
+    setStatus: function (text) {},
     totalDependencies: 0,
     monitorRunDependencies: function (left) {
       this.totalDependencies = Math.max(this.totalDependencies, left)
