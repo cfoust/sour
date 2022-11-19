1cb16445e96c6b765caceca46564e12371b12a30
Caleb Foust <cfoust@sqweebloid.com>
2022-11-18 21:05:06 -0800

    Implement basic cfg parser

--- services/go/cmd/mapdump/main.go
+++ services/go/cmd/mapdump/main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"crypto/sha256"
+	"errors"
+	"flag"
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/repeale/fp-go"
 
 	"github.com/cfoust/sour/pkg/maps"
 )
@@ -32,23 +41,249 @@ func GetChildTextures(cubes []maps.Cube) map[uint16]int {
 	return result
 }
 
+type RootFlags []string
+
+func (flags *RootFlags) String() string {
+	return "N/I"
+}
+
+func (flags *RootFlags) Set(value string) error {
+	*flags = append(*flags, value)
+	return nil
+}
+
+func FileExists(path string) bool {
+	if _, err := os.Stat(path); !os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
+
+// Search for a file in the roots, one at a time
+func SearchFile(roots []string, path string) *string {
+	for i := 0; i < len(roots); i++ {
+		needle := filepath.Join(roots[i], path)
+		if FileExists(needle) {
+			return &needle
+		}
+	}
+
+	return nil
+}
+
+type Texture struct {
+	Slot int
+	Path string
+}
+
+type Processor struct {
+	Slot int
+	// All textures referenced and their calculated slot
+	Textures []Texture
+	// File references are guaranteed to be included and do not have a slot
+	Files []string
+}
+
+func NewProcessor() *Processor {
+	processor := Processor{}
+
+	processor.Slot = 0
+	processor.Textures = make([]Texture, 0)
+	processor.Files = make([]string, 0)
+
+	return &processor
+}
+
+func (processor *Processor) AddTexture(path string) {
+	texture := Texture{}
+
+	texture.Slot = processor.Slot
+	texture.Path = path
+	processor.Slot++
+
+	processor.Textures = append(processor.Textures, texture)
+}
+
+func (processor *Processor) ResetTextures() {
+	processor.Slot = 0
+	processor.Textures = make([]Texture, 0)
+}
+
+func (processor *Processor) AddFile(path string) {
+	processor.Files = append(processor.Files, path)
+}
+
+var (
+	COMMAND_REGEX = regexp.MustCompile(`(("[^"]*")|([^\s]+))`)
+)
+
+func ParseLine(line string) []string {
+	empty := make([]string, 0)
+
+	// Split off the comments
+	parts := strings.Split(line, "//")
+
+	if len(parts) == 0 {
+		return empty
+	}
+
+	command := strings.TrimSpace(parts[0])
+
+	if len(command) == 0 {
+		return empty
+	}
+
+	// Break the command up into pieces, preserving quoted arguments
+	matches := COMMAND_REGEX.FindAllStringSubmatch(command, -1)
+
+	return fp.Map[[]string, string](
+		func(x []string) string {
+			if strings.HasPrefix(x[0], "\"") && strings.HasSuffix(x[0], "\"") {
+				return x[0][1 : len(x[0])-1]
+			}
+			return x[0]
+		},
+	)(matches)
+}
+
+func ProcessFile(roots RootFlags, processor *Processor, file string) error {
+	log.Printf("Processing %s", file)
+
+	if !FileExists(file) {
+		return errors.New(fmt.Sprintf("File %s did not exist", file))
+	}
+
+	src, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	// Before interpreting the file, we need to look for non-deterministic behavior
+	// Some .cfg files (notably single player maps) can change textures dynamically
+	dynamic := false
+	for _, line := range strings.Split(string(src), "\n") {
+		args := ParseLine(line)
+
+		if len(args) == 0 {
+			continue
+		}
+
+		// Can't do conditionals
+		if args[0] == "if" {
+			dynamic = true
+			break
+		}
+
+		for _, arg := range args {
+			if strings.HasPrefix(arg, "(") || strings.HasSuffix(arg, ")") || strings.HasPrefix(arg, "[") || strings.HasSuffix(arg, "]") {
+				dynamic = true
+				break
+			}
+		}
+	}
+
+	// Don't parse this file, attempt to parse the shimmed version
+	if dynamic {
+		hash := fmt.Sprintf("%x", sha256.Sum256(src))
+		shim := filepath.Join("shims/", hash)
+
+		log.Printf("File %s contained dynamic code. Falling back to shim %s", file, shim)
+
+		if !FileExists(shim) {
+			log.Printf("Shim %s did not exist. Creating it and exiting.", shim)
+			os.WriteFile(shim, []byte(src), 0666)
+			os.Exit(1)
+		}
+
+		return ProcessFile(roots, processor, shim)
+	}
+
+	for _, line := range strings.Split(string(src), "\n") {
+		args := ParseLine(line)
+
+		if len(args) == 0 {
+			continue
+		}
+
+		switch args[0] {
+		case "texturereset":
+			processor.ResetTextures()
+
+		case "exec":
+			if len(args) > 1 {
+				execPath := args[1]
+
+				resolved := SearchFile(roots, execPath)
+
+				if resolved == nil {
+					log.Printf("Could not find %s", execPath)
+				} else {
+					err := ProcessFile(roots, processor, *resolved)
+					if err != nil {
+						return err
+					}
+				}
+			}
+
+		case "include":
+			if len(args) > 1 {
+				processor.AddFile(args[1])
+			}
+
+		case "texture":
+			if len(args) > 2 {
+				flag := args[1]
+
+				// Other shader inputs don't get slots
+				if flag == "0" {
+					processor.AddTexture(args[2])
+				} else {
+					processor.AddFile(args[2])
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
 func main() {
-	args := os.Args[1:]
+	var roots RootFlags
+
+	flag.Var(&roots, "root", "Specify an explicit asset root directory. Roots are searched in order of appearance.")
+	flag.Parse()
+
+	args := flag.Args()
 
 	if len(args) != 1 {
-		log.Fatal("Please provide at least one argument.")
-		return
+		log.Fatal("You must provide only a single argument.")
 	}
 
 	filename := args[0]
+	extension := filepath.Ext(filename)
+
+	if extension != ".ogz" && extension != ".cgz" {
+		log.Fatal("Map must end in .ogz or .cgz")
+	}
+
 	_map, err := maps.LoadMap(filename)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to parse map file")
 	}
 
-	textureRefs := GetChildTextures(_map.Cubes)
-	for k, v := range textureRefs {
-		log.Printf("[%d]=%d", k, v)
+	GetChildTextures(_map.Cubes)
+
+	// Always load the default map settings
+	default_path := SearchFile(roots, "data/default_map_settings.cfg")
+
+	if default_path == nil {
+		log.Fatal("Root with data/default_map_settings.cfg not provided")
+	}
+
+	processor := NewProcessor()
+	err = ProcessFile(roots, processor, *default_path)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
--- services/go/go.mod
+++ services/go/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/codecat/go-enet v0.0.0-20201213053919-8c1bf6ac65fa // indirect
 	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
 	github.com/klauspost/compress v1.10.3 // indirect
+	github.com/repeale/fp-go v0.11.1 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
