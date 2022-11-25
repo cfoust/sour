5574877ed78ae38ae2282ff3a80307f8bd37d88e
Caleb Foust <cfoust@sqweebloid.com>
2022-11-25 11:39:53 -0800

    Generate JSON index for base game

--- services/game/assets/base.py
+++ services/game/assets/base.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
         maps = list(map(lambda a: "roots/base/packages/base/%s.ogz" % a, args[1:]))
 
     outdir = os.getenv("ASSET_OUTPUT_DIR", "output/")
+    prefix = os.getenv("PREFIX")
     os.makedirs(outdir, exist_ok=True)
 
     roots = [
@@ -21,7 +22,7 @@ if __name__ == "__main__":
         "roots/base",
     ]
 
-    hashes: List[Tuple[str, str]] = []
+    mods: List[package.Mod] = []
 
     # Build base
     with open("base.list", "r") as f:
@@ -34,14 +35,29 @@ if __name__ == "__main__":
             mappings.append(mapping)
 
         _hash = package.build_bundle(mappings, outdir, compress_images=False)
-        hashes.append(("base", _hash))
+        mods.append(
+            package.Mod(
+                name="base",
+                bundle=_hash
+            )
+        )
+
+    game_maps: List[package.GameMap] = []
 
     for _map in maps:
         base, _ = path.splitext(path.basename(_map))
         print("Building %s" % base)
-        _hash = package.build_map_bundle(_map, roots, outdir)
-        hashes.append((base, _hash))
+        map_bundle = package.build_map_bundle(_map, roots, outdir)
+        game_maps.append(
+            package.GameMap(
+                name=base,
+                bundle=map_bundle.bundle,
+                image=map_bundle.image,
+                description="""
+Base game map %s as it appeared in game version r6584.
+""" % base,
+                aliases=[]
+            )
+        )
 
-    with open(path.join(outdir, ".index"), "w") as f:
-        for _map, _hash in hashes:
-            f.write("%s %s\n" % (_map, _hash))
+    package.dump_index(game_maps, mods, outdir, prefix)
