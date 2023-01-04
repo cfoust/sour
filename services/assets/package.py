85e98425fda67188410a65a4f72eaf1d2b455036
Caleb Foust <cfoust@sqweebloid.com>
2023-01-04 20:45:28 +0000

    More prep for assetgeddon round two

--- services/assets/package.py
+++ services/assets/package.py
@@ -25,10 +25,17 @@ class Asset(NamedTuple):
     path: str
 
 
-class Mod(NamedTuple):
-    name: str
-    # A list of asset IDs
+class Bundle(NamedTuple):
+    id: str
     assets: List[Asset]
+
+
+class Mod(NamedTuple):
+    # A mod's id is equivalent to its bundle id
+    id: str
+    name: str
+    image: Optional[str]
+    description: str
 
 
 class GameMap(NamedTuple):
@@ -48,6 +55,15 @@ class GameMap(NamedTuple):
     image: Optional[str]
     # A description of the map that can be shown to the user.
     description: str
+
+
+class Model(NamedTuple):
+    # The hash of the model and all of its contents
+    # Refers to a bundle
+    id: str
+    # The path that Sauer would use, a reference to a directory in
+    # package/models e.g. skull/blue
+    name: str
 
 
 def hash_string(string: str) -> str:
@@ -107,6 +123,171 @@ def sizeof_fmt(num, suffix="B"):
     return f"{num:.1f}Yi{suffix}"
 
 
+def combine_bundle(data_file: str, js_file: str, dest: str):
+    """
+    Given the output of Emscripten's file packager, make a single file with the
+    file list and data combined.
+    """
+
+    data = open(data_file, 'rb').read()
+    js = open(js_file, 'r').read()
+    package = re.search('loadPackage\((.+)\)', js)
+
+    if not package:
+        raise Exception("Failed to find loadPackage in %s" % js_file)
+
+    # We could compute these directories from the file list alone, but I'm
+    # lazy.
+    paths = []
+    for directory in re.finditer('createPath...(.+), true', js):
+        paths.append(json.loads('[%s]' % directory[1][:-6]))
+
+    directories = json.dumps(paths)
+    metadata = package[1]
+
+    with open(dest, 'wb') as out:
+        out.write(len(directories).to_bytes(4, 'big'))
+        out.write(bytes(directories, 'utf-8'))
+        out.write(len(metadata).to_bytes(4, 'big'))
+        out.write(bytes(metadata, 'utf-8'))
+        out.write(data)
+
+    os.remove(data_file)
+    os.remove(js_file)
+
+
+
+def build_sour_bundle(
+    bundle: Bundle,
+    outdir: str,
+    compress_images: bool = True,
+    print_summary: bool = False
+) -> str:
+    """
+    Given a list of files and a destination, build a Sour-compatible bundle.
+    Images are compressed by default, but you can disable this with
+    `compress_images`.
+    """
+
+    # We may remap files after conversion
+    cleaned: List[Mapping] = []
+
+    sour_target = path.join(outdir, "%s.sour" % bundle.id)
+
+    if path.exists(sour_target):
+        return bundle.id
+
+    sizes: List[Tuple[str, int]] = []
+
+    for asset in bundle.assets:
+        _in = path.join(outdir, asset.id)
+        out = asset.path
+        _, extension = path.splitext(_in)
+
+        if not path.exists(_in):
+            continue
+
+        size = path.getsize(_in)
+
+        sizes.append((_in, size))
+
+        # We can only compress certain file types
+        if (
+            extension not in [".dds", ".jpg", ".png"] or
+            size < 128000 or
+            not compress_images
+        ):
+            cleaned.append((_in, out))
+            continue
+
+        os.makedirs("working/", exist_ok=True)
+
+        # If multiple bundles rely on the same converted image, we don't want
+        # to redo the calculation.
+        compressed = path.join(
+            "working/",
+            "%s%s" % (
+                hash_string(_in),
+                extension
+            )
+        )
+
+        if path.exists(compressed):
+            cleaned.append((compressed, out))
+            continue
+
+        # Make the image 1/4 of the size using ImageMagick
+        for _from, _to in [
+                (_in, compressed),
+                (compressed, compressed)
+        ]:
+            subprocess.run(
+                [
+                    "convert",
+                    _from,
+                    "-resize",
+                    "50%",
+                    _to
+                ],
+                check=True
+            )
+
+        cleaned.append((compressed, out))
+
+    if print_summary:
+        sizes = list(reversed(sorted(sizes, key=lambda a: a[1])))
+
+        for _in, size in sizes:
+            print(f"{_in} {sizeof_fmt(size)}")
+
+    js_file = "/tmp/preload_%s.js" % bundle.id
+    data_file = "/tmp/%s.data" % bundle.id
+
+    result = subprocess.run(
+        [
+            "python3",
+            "%s/upstream/emscripten/tools/file_packager.py" % os.getenv('EMSDK', '/emsdk'),
+            data_file,
+            "--use-preload-plugins",
+            "--preload",
+            *list(map(
+                lambda v: "%s@%s" % (v[0], v[1]),
+                cleaned
+            )),
+        ],
+        check=True,
+        capture_output=True
+    )
+
+    with open(js_file, 'wb') as f: f.write(result.stdout)
+
+    combine_bundle(data_file, js_file, sour_target)
+    return bundle.id
+
+
+def build_desktop_bundle(outdir: str, bundle: Bundle):
+    added: Set[str] = set()
+    zip_path = path.join(outdir, "%s.desktop" % bundle.id)
+    with zipfile.ZipFile(
+        zip_path,
+        'w',
+        compression=zipfile.ZIP_DEFLATED,
+        compresslevel=9
+    ) as desktop:
+        for asset in bundle.assets:
+            _in = path.join(outdir, asset.id)
+            _out = asset.path
+
+            if _out in added:
+                continue
+
+            with desktop.open(_out, 'w') as outfile:
+                with open(_in, 'rb') as infile:
+                    outfile.write(infile.read())
+
+            added.add(_out)
+
+
 def get_map_files(map_file: str, roots: List[str]) -> List[Mapping]:
     """
     Get all of the files referenced by a Sauerbraten map.
@@ -144,12 +325,21 @@ def get_map_files(map_file: str, roots: List[str]) -> List[Mapping]:
 class Packager:
     outdir: str
     assets: Set[str]
+
+    bundles: List[Bundle]
     maps: List[GameMap]
+    models: List[Model]
+    mods: List[Mod]
+    textures: List[Asset]
 
     def __init__(self, outdir: str):
         self.outdir = outdir
         self.assets = set()
+        self.bundles = []
         self.maps = []
+        self.models = []
+        self.mods = []
+        self.textures = []
 
 
     def build_asset(self, file: Mapping, compress_images: bool = True) -> Optional[Asset]:
@@ -321,48 +511,48 @@ class IndexMod(NamedTuple):
     assets: List[IndexAsset]
 
 
-def dump_index(maps: List[GameMap], mods: List[Mod], assets: List[str], outdir: str, prefix = ''):
-    index = '%s.index.json' % prefix
+# def dump_index(maps: List[GameMap], mods: List[Mod], assets: List[str], outdir: str, prefix = ''):
+    # index = '%s.index.json' % prefix
 
-    lookup: Dict[str, int] = {}
-    for i, asset in enumerate(assets):
-        lookup[asset] = i
+    # lookup: Dict[str, int] = {}
+    # for i, asset in enumerate(assets):
+        # lookup[asset] = i
 
-    def replace_asset(asset: Asset) -> IndexAsset:
-        return IndexAsset(
-            id=lookup[asset.id],
-            path=asset.path,
-        )
+    # def replace_asset(asset: Asset) -> IndexAsset:
+        # return IndexAsset(
+            # id=lookup[asset.id],
+            # path=asset.path,
+        # )
 
-    index_maps: List[IndexMap] = list(map(
-        lambda map_: IndexMap(
-            id=map_.id,
-            name=map_.name,
-            ogz=lookup[map_.ogz],
-            assets=list(map(replace_asset, map_.assets)),
-            image=map_.image,
-            description=map_.description,
-        ),
-        maps
-    ))
+    # index_maps: List[IndexMap] = list(map(
+        # lambda map_: IndexMap(
+            # id=map_.id,
+            # name=map_.name,
+            # ogz=lookup[map_.ogz],
+            # assets=list(map(replace_asset, map_.assets)),
+            # image=map_.image,
+            # description=map_.description,
+        # ),
+        # maps
+    # ))
 
-    index_mods: List[IndexMod] = list(map(
-        lambda mod: IndexMod(
-            name=mod.name,
-            assets=list(map(replace_asset, mod.assets)),
-        ),
-        mods
-    ))
+    # index_mods: List[IndexMod] = list(map(
+        # lambda mod: IndexMod(
+            # name=mod.name,
+            # assets=list(map(replace_asset, mod.assets)),
+        # ),
+        # mods
+    # ))
 
-    with open(path.join(outdir, index), 'w') as f:
-        f.write(json.dumps(
-            {
-                'assets': assets,
-                'maps': list(map(lambda _map: _map._asdict(), index_maps)),
-                'mods': list(map(lambda mod: mod._asdict(), index_mods)),
-            },
-            indent=4
-        ))
+    # with open(path.join(outdir, index), 'w') as f:
+        # f.write(json.dumps(
+            # {
+                # 'assets': assets,
+                # 'maps': list(map(lambda _map: _map._asdict(), index_maps)),
+                # 'mods': list(map(lambda mod: mod._asdict(), index_mods)),
+            # },
+            # indent=4
+        # ))
 
 
 if __name__ == "__main__": pass
