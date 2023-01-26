164ac41714fe48a1202f8d0f72674212b79bf0be
Caleb Foust <cfoust@sqweebloid.com>
2023-01-26 22:56:24 +0000

    Consent flow working

--- services/go/svc/cluster/service/cs.go
+++ services/go/svc/cluster/service/cs.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"crypto/sha256"
 	"fmt"
 	"time"
 
@@ -36,35 +37,74 @@ func sendServerInfo(u *User, domain string) {
 const DEFAULT_TIMEOUT = 5 * time.Second
 
 const INITIAL_SCRIPT = `
-echo test
+authkey %s _DO_NOTHING_ %s
+//saveauthkeys
+
+mapstart = [
+	if (>= (strstr (getservauth) "%s") 0) [
+		do (getservauth)
+	]
+]
+
+servcmd %s
 `
 
+func getDomain(authDomain string) string {
+	return fmt.Sprintf("%s-autoexec", authDomain)
+}
+
 func (c *Cluster) waitForConsent(ctx context.Context, u *User) error {
+	logger := u.Logger()
+	domain := getDomain(c.authDomain)
+	private := fmt.Sprintf("%x", sha256.Sum256([]byte(fmt.Sprintf("private-%d", time.Now()))))[:10]
+	public := fmt.Sprintf("%x", sha256.Sum256([]byte(fmt.Sprintf("public-%d", time.Now()))))[:10]
+
+	script := fmt.Sprintf(
+		INITIAL_SCRIPT,
+		public,
+		domain,
+		private,
+		public,
+	)
+
+	if len(script) > 260 {
+		logger.Fatal().Msgf("script too long %d", len(script))
+	}
+
 	sendServerInfo(
 		u,
-		INITIAL_SCRIPT,
+		script,
 	)
 	u.From.NextTimeout(ctx, DEFAULT_TIMEOUT, game.N_CONNECT)
 
 	serverInfo := u.To.Intercept(game.N_SERVINFO)
+	servCmd := u.From.Intercept(game.N_SERVCMD)
 	for {
 		select {
 		case <-u.Context().Done():
 			return nil
-		case msg := <- serverInfo.Receive():
+		case msg := <-serverInfo.Receive():
 			info := msg.Message.Contents().(*game.ServerInfo)
-			info.Domain = INITIAL_SCRIPT
+			info.Domain = script
 			p := game.Packet{}
 			p.PutInt(int32(game.N_SERVINFO))
 			p.Put(*info)
 			msg.Replace(p)
+		case msg := <-servCmd.Receive():
+			cmd := msg.Message.Contents().(*game.ServCMD)
+			logger.Info().Msgf("%+v", cmd)
+			if cmd.Command != public {
+				msg.Pass()
+				continue
+			}
+			logger.Info().Msg("user consented")
 		}
 	}
 }
 
 func (c *Cluster) setupCubeScript(ctx context.Context, u *User) error {
 	logger := u.Logger()
-	domain := fmt.Sprintf("%s-autoexec", c.authDomain)
+	domain := getDomain(c.authDomain)
 
 	// First determine whether they already have an autoexec key
 	sendServerInfo(
